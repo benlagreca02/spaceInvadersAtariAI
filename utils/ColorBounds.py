9f7c4babf5feae95ac080e78e2551ead26ee6e1e
Benjamin LaGreca <benlagreca02@gmail.com>
2023-10-18 20:36:46 -0400

    added 2nd K dimension instead of assume square, tweaked color hardcode values, made dict for all alien types

--- utils/ColorBounds.py
+++ utils/ColorBounds.py
@@ -1,17 +1,17 @@
-
 # Class and objects for color masking aliens out
 # The idea is that this will be used to "quick and dirty" collect lots of pics of the aliens, which will then be
 # manually sorted through. Just not consistent enough to feed directly into some game-playing ai...
 
 class ColorBounds:
-    def __init__(self, lh, uh, ls, us, lv, uv, k, sig):
+    def __init__(self, lh, uh, ls, us, lv, uv, kx, ky, sig=0):
         self.uh = uh
         self.lh = lh
         self.us = us
         self.ls = ls
         self.uv = uv
         self.lv = lv
-        self.k = k
+        self.kx = kx
+        self.ky = ky
         self.sig = sig
 
     def get_hue_bounds(self):
@@ -26,12 +26,16 @@ class ColorBounds:
     def get_upper_bounds(self):
         return self.uh, self.us, self.uv
 
-    def get_k(self):
-        return self.k
+    def get_k_dims(self):
+        return self.kx, self.ky
 
     def get_sigma(self):
         return self.sig
 
 
-BLUE_ALIEN = ColorBounds(120, 122, 183, 228, 83, 104, 45, 0)
-YELLOW_ALIEN = ColorBounds(16, 23, 198, 230, 116, 148, 59, 0)
+MIN_ALIEN_SIZE = 70
+# constant values for roughly detecting the color of the aliens
+BLUE_ALIEN = ColorBounds(119, 120, 183, 247, 63, 95, 31, 29)
+YELLOW_ALIEN = ColorBounds(17, 22, 172, 246, 91, 132, 59, 59)
+
+ALIENS = {"blue": BLUE_ALIEN, "yellow": YELLOW_ALIEN}
