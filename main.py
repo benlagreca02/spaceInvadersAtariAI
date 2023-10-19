b3c4b223b0ec369477fa3f2254f99ba7b9206e8e
Benjamin LaGreca <benlagreca02@gmail.com>
2023-10-18 20:29:35 -0400

    moved 'gather test pics' idea to ScreenRipper.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import time
 
 from utils import ColorBounds, ImageProcessor
-from utils.screenRipper import ScreenRipper
+from utils.ScreenRipper import ScreenRipper
 import cv2
-
-WINDOW_TITLE = "simple64    build: f7bab16"
 
 
 def small_show_and_wait(window_name, img):
@@ -13,21 +11,10 @@ def small_show_and_wait(window_name, img):
     cv2.waitKey(0)
 
 
-# RUNS INFINITELY, takes a screenshot named "<starting_num>.png" and puts in testCaptures folder
-def gather_test_pics(starting_num, secs_between_screenshots):
-    myRipper = ScreenRipper(WINDOW_TITLE)
-    i = starting_num
-    while True:
-        screen = myRipper.get_screen()
-        cv2.imwrite(f"testCaptures/{i}.png", screen)
-        i += 1
-        print(f"Saved picture {i}...")
-        time.sleep(secs_between_screenshots)
-
-
+# right now, just boxes around some blue aliens with a blue rectangle and shows the steps
 if __name__ == '__main__':
     img = cv2.imread("testCaptures/9.png")
     small_show_and_wait("pic", img)
-    img_boxes = ImageProcessor.box_aliens(ColorBounds.BLUE_ALIEN, img, [0xFF, 0xCC, 0x00])
+    img_boxes = ImageProcessor.draw_boxes_around_alien_color(ColorBounds.BLUE_ALIEN, img, [0xFF, 0xCC, 0x00])
     small_show_and_wait("boxes", img_boxes)
 
