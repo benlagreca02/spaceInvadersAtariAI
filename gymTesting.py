3375cdb82af325759e3bd88a3686fae4b70cef7b
Benjamin LaGreca <benlagreca02@gmail.com>
2023-11-18 16:21:20 -0500

    Using stable_baselines3, training LunarLander with DQN

--- gymTesting.py
+++ gymTesting.py
@@ -1,18 +1,31 @@
-import gym
+import gymnasium as gym
+from stable_baselines3 import DQN, A2C
+from stable_baselines3.common.evaluation import evaluate_policy
 
 if __name__ == '__main__':
-    # render mode makes it so we see it
-    # env = gym.make('ALE/SpaceInvaders-v5', render_mode='human')
-    env = gym.make('LunarLander-v2', render_mode='human')
 
-    # no idea
-    env.action_space.seed(69)
+    TOTAL_TIMESTEPS = 500000
 
-    observation, info = env.reset(seed=69)
+    # make space invaders environment
+    print("Making environment")
+    env = gym.make('LunarLander-v2', render_mode='rgb_array')
 
-    for _ in range(1000):
-        observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-        if terminated or truncated:
-            observation, info = env.reset()
+    # instantiate agent
+    print("Instantiating agent")
+    model = DQN("MlpPolicy", env, verbose=0)
 
-    env.close()
+    # train the agent w/ prog. bar
+    print("Training...")
+    model.learn(total_timesteps=TOTAL_TIMESTEPS, progress_bar=True)
+
+    model.save(f'dqn_spaceInvaders_{TOTAL_TIMESTEPS}_steps')
+
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+    # "enjoy trained agent"
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+    for _ in range(5000):
+        action, _state = model.predict(obs, deterministic=True)
+        obs, reward, done, info = vec_env.step(action)
+        vec_env.render("human")
