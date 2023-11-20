ff43c6ab0f5c781b0abf79c43ba29de79a0df0af
Benjamin LaGreca <benlagreca02@gmail.com>
2023-11-20 15:10:33 -0500

    cleaning

--- loadAndRunAgent.py
+++ loadAndRunAgent.py
@@ -3,28 +3,22 @@ import time
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3 import DQN, A2C
 
-
-# just a copy paste from the other file...
-# could be cleaned up a lot I know, but this is still just 
-# the 'playing aorund with things' phase of stuff
-
-TOTAL_TIMESTEPS = 500_000
 ENV_NAME = 'ALE/SpaceInvaders-v5'
+
+TOTAL_TIMESTEPS = 1_000_000
+BUFFER_SIZE = 100_000
+VEC_STACK=12
 
 # pick between MlpPolicy, CnnPolicy, and MultiInputPolicy
 # docs say to use Cnn with image inputs
 DQN_POLICY = 'CnnPolicy'
-NUM_ENVS = 1
 
-# this is the 'replay buffer' size, too large and we won't be able to
-# instantiate the agent
-BUFFER_SIZE = 100_000
 
 if __name__ == "__main__":
 
     env = make_atari_env('SpaceInvadersNoFrameskip-v4', n_envs=NUM_ENVS)
 
-    model = DQN.load(f'{DQN_POLICY}_ReplaySize{BUFFER_SIZE}_NumTimesteps{TOTAL_TIMESTEPS}', env=env)
+    model = DQN.save(f'{DQN_POLICY}_ReplaySize{BUFFER_SIZE}_NumTimesteps{TOTAL_TIMESTEPS}_VecStack{VEC_STACK}', env=env)
 
     vec_env = model.get_env()
     obs=vec_env.reset()
--- trainModel.py
+++ trainModel.py
@@ -8,17 +8,12 @@ from stable_baselines3.common.vec_env import VecFrameStack
 
 if __name__ == '__main__':
 
-    TOTAL_TIMESTEPS = 1_000_000
     ENV_NAME = 'ALE/SpaceInvaders-v5'
-
-    # pick between MlpPolicy, CnnPolicy, and MultiInputPolicy
-    # docs say to use Cnn with image inputs
     DQN_POLICY = 'CnnPolicy'
-    NUM_ENVS = 1
 
-    # this is the 'replay buffer' size, too large and we won't be able to
-    # instantiate the agent
-    BUFFER_SIZE = 100_000
+    TOTAL_TIMESTEPS = 1_000_000
+    BUFFER_SIZE = 100_000  # if too large, can't allocate that much memory
+    VEC_STACK = 12
 
     print(f"Making environment: {ENV_NAME}")
 
@@ -27,10 +22,10 @@ if __name__ == '__main__':
 
     # we use 'make_atari_env' rather than 'gym.make()' because make_atari does some
     # preprocessing on the env. to make it less data intensive
-    env = make_atari_env('SpaceInvadersNoFrameskip-v4', n_envs=NUM_ENVS)
+    env = make_atari_env('SpaceInvadersNoFrameskip-v4')
 
     # I wonder if this will break anything
-    env = VecFrameStack(env, n_stack=12)
+    env = VecFrameStack(env, n_stack=VEC_STACK)
 
     # instantiate agent
     print("Instantiating agent")
@@ -44,8 +39,10 @@ if __name__ == '__main__':
     model.learn(total_timesteps=TOTAL_TIMESTEPS, progress_bar=True)
 
     print("Saving model")
-    model.save(f'{DQN_POLICY}_ReplaySize{BUFFER_SIZE}_NumTimesteps{TOTAL_TIMESTEPS}')
+    model.save(f'{DQN_POLICY}_ReplaySize{BUFFER_SIZE}_NumTimesteps{TOTAL_TIMESTEPS}_VecStack{VEC_STACK}')
 
     # for evaluating
     # mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+    print("Done!")
 
